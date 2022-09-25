7e31e91c25c1548c1e2c7513bdd34dba84be6d5a
DarkApe <darkape.eth@gmail.com>
2022-09-25 18:26:02 -0300

    10. Withdraw Tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -12,6 +12,7 @@ contract Exchange {
 	mapping(address => mapping(address => uint256)) public tokens;
 
 	event Deposit(address token, address user, uint256 amount, uint256 balance);
+	event Withdraw(address token, address user, uint256 amount, uint256 balance);
 
 	constructor(address _feeAccount, uint256 _feePercent){
 		feeAccount = _feeAccount;
@@ -33,6 +34,22 @@ contract Exchange {
 
 	}
 
+	// Withdraw Tokens
+	function withdrawToken(address _token, uint256 _amount) public {
+		// Ensure user has enough tokens to withdraw
+		require(tokens[_token][msg.sender] >= _amount);
+
+		// Transfer tokens to user
+		Darktoken(_token).transfer(msg.sender, _amount);
+
+		// // Update user balance
+		tokens [_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+
+		// // Emit event
+		emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);		
+	}
+
+
 	// Check Balances  
 	function balanceOf(address _token, address _user) // its a wrapper function in solidity that checks values out of a mapping
 		public
@@ -42,11 +59,14 @@ contract Exchange {
 		return tokens[_token][_user];
 	}
 
+
+
+
 }
 
 
 
-// Withdraw Tokens
+
 
 
 // Make Orders
