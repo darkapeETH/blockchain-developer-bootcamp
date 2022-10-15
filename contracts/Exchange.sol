2c6ba60f05cd7ad3eccbbb8e330d3857678acc14
DarkApe <darkape.eth@gmail.com>
2022-10-15 01:36:40 -0300

    12. Fill Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -13,11 +13,13 @@ contract Exchange {
 	mapping(uint256 => _Order) public orders;
 	uint256 public orderCount;
 	mapping(uint256 => bool) public orderCancelled; // true or false (boolean / bool)
+	mapping(uint256 => bool) public orderFilled; 
 
 	event Deposit(address token, address user, uint256 amount, uint256 balance);
 	event Withdraw(address token, address user, uint256 amount, uint256 balance);
 	event Order(uint256 id, address user, address tokenGet,	uint256 amountGet, address tokenGive, uint256 amountGive, uint256 timestamp);
 	event Cancel(uint256 id, address user, address tokenGet, uint256 amountGet, address tokenGive, uint256 amountGive, uint256 timestamp);
+	event Trade(uint256 id, address user, address tokenGet, uint256 amountGet, address tokenGive, uint256 amountGive, address creator, uint256 timestamp);
 
 
 	// Orders Mapping / Struct (a way to model the order)
@@ -84,7 +86,7 @@ contract Exchange {
 		require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
 
 		// Instantiate a new order
-		orderCount = orderCount + 1;
+		orderCount++; // ++ is the same that making orderCount = orderCount + 1
 		orders[orderCount] = _Order(
 			orderCount, 
 			msg.sender,
@@ -112,7 +114,7 @@ contract Exchange {
 
 		// Fetching the order
 			// Read the order
-		_Order storage _order= orders[_id]; //We always have to declare a data type when we are declaring a variable in solidity
+		_Order storage _order = orders[_id]; //We always have to declare a data type when we are declaring a variable in solidity
 											//but with solidity we have to tell it that we are pulling this order out of storage from the mapping. This is actually a Storage struct that is pulled out of the mapping that is writen to the blockchain.
 		// Ensure the caller of the function is the owner of the order
 		require(address(_order.user) == msg.sender); // one = assigns values, vs two == compares values.
@@ -134,8 +136,70 @@ contract Exchange {
 			block.timestamp
 		);
 	}
-}
-
 // Fill Orders
+	
+	function fillOrder(uint256 _id) public {
 
-// Charge Fees
+		// 1. Must be valid orderId
+		require(_id >0 && _id <= orderCount, "Order does not exist");	
+		// 2. Order can't be filled
+		require(!orderFilled[_id]);
+		// 3. Order can't be cancelled
+		require(!orderCancelled[_id]);
+
+		// Fetching the order
+		_Order storage _order = orders[_id];
+
+		// Execute the trade
+		_trade(
+			_order.id, 
+			_order.user, 
+			_order.tokenGet,
+			_order.amountGet,
+			_order.tokenGive,
+			_order.amountGive
+			);
+		// Mark order as filled
+		orderFilled[_order.id] = true;
+
+	}
+
+	function _trade(
+		uint256 _orderId,
+		address _user,
+		address _tokenGet,
+		uint256 _amountGet,
+		address _tokenGive,
+		uint256 _amountGive
+
+		) internal {
+
+			//Fee is paid by the user who filled the order (msg.sender)
+			//Fee is deducted from _amountGet
+			uint256 _feeAmount = (_amountGet * feePercent)/100;
+
+			//Execute the trade
+			// msg.sende is the user who filled the order, while _user is who created the order
+			tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender] - (_amountGet + _feeAmount);
+			tokens[_tokenGet][_user] = tokens[_tokenGet][_user] + _amountGet;
+
+			// Charge fees
+			tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount] + _feeAmount;
+
+			tokens[_tokenGive][_user] = tokens[_tokenGive][_user] - _amountGive;
+			tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender] + _amountGive;
+
+
+		// Emit event
+		emit Trade(
+			_orderId,
+			msg.sender,
+			_tokenGet,
+			_amountGet,
+			_tokenGive,
+			_amountGive,
+			_user,
+			block.timestamp
+		);
+	}
+}
